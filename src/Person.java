937e883fae491e23e55e41080cf67706f5a22fe7
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2022-09-07 23:02:16 +0530

    static methods

--- src/Person.java
+++ src/Person.java
@@ -14,6 +14,17 @@ public class Person {
     static private Company company = new Company();
     private NewAgency newAgency;
     private static final double PI = 3.14;
+    public static int[] nums = initNums();
+
+    public static int[] initNums(){
+        int[] nums = new int[5];
+        nums[0] = 1;
+        nums[1] = 3;
+        nums[2] = 4;
+        nums[3] = 100;
+        nums[4] = 24;
+        return nums;
+    }
 
     public static void varArgsTestMethod(String... words){
         System.out.println(Arrays.toString(words));
@@ -26,5 +37,6 @@ public class Person {
         //static variable
         System.out.println(PI);
         System.out.println(Person.PI);
+        System.out.println(Arrays.toString(nums));
     }
 }
