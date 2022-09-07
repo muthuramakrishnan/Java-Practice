8e5e1257427310f6db0a50b7d416bf762dba4707
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2022-09-07 21:43:42 +0530

    Initial commit

---
+++ src/NewAgency.java
@@ -0,0 +1,7 @@
+import business.Company;
+//import business.FactoryWorker; --> this cannot be accessed here because, the class is package protected
+
+public class NewAgency {
+    Company company;
+//    FactoryWoker factoryWoker;
+}
---
+++ src/business/FactoryWorker.java
@@ -0,0 +1,6 @@
+package business;
+
+//if no access specifier is used then by default it is package protected
+//only Company class can access this class
+class FactoryWorker {
+}
